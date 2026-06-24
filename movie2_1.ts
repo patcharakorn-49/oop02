74852b61bfc4cccba06e6c13a0d7bce7973da3c8
N. <patcharakorn49@gmail.com>
2026-06-24 16:32:40 +0700

    ส่งงาน Workshop2_1

---
+++ movie2_1.ts
@@ -0,0 +1,8 @@
+let age: number = 20 ;
+console.log("age: ",age );
+if (age < 12) {
+    console.log("Movie price: $5");
+}else if (age <=60){
+    console.log("Movie price: $10");
+}else
+    console.log("Movie price : $7 ");
